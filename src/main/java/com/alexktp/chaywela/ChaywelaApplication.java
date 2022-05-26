1b6f750ee69cf388eaf25bf5d06707291c5e4632
alexktp <alexandre.malachin@gmail.com>
2022-05-26 10:51:24 +0200

    get remote

---
+++ src/main/java/com/alexktp/chaywela/ChaywelaApplication.java
@@ -0,0 +1,13 @@
+package com.alexktp.chaywela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaywelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaywelaApplication.class, args);
+	}
+
+}
